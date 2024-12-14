14a9b1cc31f0a0fa6c8de21c4c8b0182f7347c12
tayyabashahzad12 <tayyabashahzad12@gmail.com>
2024-12-14 01:07:06 -0800

    fix: mjs code

--- exercises/exercise1/script.mjs
+++ exercises/exercise1/script.mjs
@@ -1,16 +1,19 @@
 const alertFunction = () => {
     const number = prompt("Enter any number to generate its multiplication table");
 
-if (number) {
-    document.write(`Multiplication table for ${number}:<br>`);
-    let counter = 1;
-    while (counter <= 10) {
-        document.write(` ${number} x ${counter} = ${number * counter}<br>`);
-        counter++;
+    if (number) {
+        document.write(`Multiplication table for ${number}:<br>`);
+        document.write(`${number} x 1 = ${number * 1}<br>`);
+        document.write(`${number} x 2 = ${number * 2}<br>`);
+        document.write(`${number} x 3 = ${number * 3}<br>`);
+        document.write(`${number} x 4 = ${number * 4}<br>`);
+        document.write(`${number} x 5 = ${number * 5}<br>`);
+        document.write(`${number} x 6 = ${number * 6}<br>`);
+        document.write(`${number} x 7 = ${number * 7}<br>`);
+        document.write(`${number} x 8 = ${number * 8}<br>`);
+        document.write(`${number} x 9 = ${number * 9}<br>`);
+        document.write(`${number} x 10 = ${number * 10}<br>`);
+    } else {
+        document.write("You did not enter a number.");
     }
-}
-
-if (!number) {
-    document.write("You did not enter a number.");
-}
 }
